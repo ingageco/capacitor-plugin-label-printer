c9533f0ecc1b180529086427b5beebde8926215d
Jay Hughes <jay@weingage.com>
2024-09-23 11:28:10 -0400

    wlan only

--- ios/Sources/CapacitorPluginLabelPrinterPlugin/CapacitorPluginLabelPrinterPlugin.m
+++ ios/Sources/CapacitorPluginLabelPrinterPlugin/CapacitorPluginLabelPrinterPlugin.m
@@ -2,7 +2,7 @@
 #import <Capacitor/Capacitor.h>
 
 CAP_PLUGIN(CapacitorPluginLabelPrinterPlugin, "CapacitorPluginLabelPrinter",
-     CAP_PLUGIN_METHOD(getBrotherPrinters, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(getBrotherPrinters, CAPPluginReturnPromise);
     CAP_PLUGIN_METHOD(getPrinters, CAPPluginReturnPromise);
     CAP_PLUGIN_METHOD(cancelPrinterSearch, CAPPluginReturnPromise);
     CAP_PLUGIN_METHOD(getPrinterStatus, CAPPluginReturnPromise);
